3e164f3bf866e7c18bc2855ae644951b37e80a91
Felipe Alexandre <felipe.silva@petlove.com.br>
2022-04-21 17:38:43 -0300

    pageable ans filters for course

--- src/main/java/com/ead/course/services/CourseService.java
+++ src/main/java/com/ead/course/services/CourseService.java
@@ -1,8 +1,10 @@
 package com.ead.course.services;
 
 import com.ead.course.models.CourseModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -10,5 +12,5 @@ public interface CourseService {
     void delete(CourseModel courseModel);
     CourseModel save(CourseModel courseModel);
     Optional<CourseModel> findById(UUID id);
-    List<CourseModel> findAll();
+    Page<CourseModel> findAll(Specification<CourseModel> spec, Pageable pageable);
 }
